37932f1108ed3e5b630c44c7fd98d63863e00227
Iriska7266 <danik02022@yandex.ru>
2026-05-28 22:44:59 +0500

    parser: added date-time filter

--- Parser/parser.py
+++ Parser/parser.py
@@ -2,6 +2,8 @@ import telethon
 
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+from typing import Optional
 
 load_dotenv()
 
@@ -13,19 +15,34 @@ class TGParser:
     def __init__(self):
         self.client = telethon.TelegramClient("news_parser", API_KEY, API_HASH)
 
-    async def parse_channel(self, channel, limit=50):
+    async def parse_channel(self, channel: str,
+                            start_date: datetime = None,
+                            end_date: datetime = None,
+                            limit: Optional[int] = None):
+
+        if start_date and start_date.tzinfo is None:
+            start_date = start_date.replace(tzinfo=timezone.utc)
+        if end_date and end_date.tzinfo is None:
+            end_date = end_date.replace(tzinfo=timezone.utc)
+
+        offset = end_date if end_date else None
+
         rows = []
-        async for message in self.client.iter_messages(channel, limit=limit):
+
+        async for message in self.client.iter_messages(channel, limit=limit, offset_date=offset):
+            if start_date and message.date < start_date:
+                break
+
             if not message.text:
                 continue
 
-            rows.append({
-                "Date": message.date.strftime("%Y-%m-%d %H:%M:%S"),
-                "Body": message.text.replace("\n", " "),
-                "Link": f"https://t.me/{channel}/{message.id}",
-                "Views": message.views or 0,
-                "Forwards": message.forwards or 0
-            })
+            rows.append([
+                message.date.strftime("%Y-%m-%d %H:%M:%S"),
+                message.text.replace("\n", " "),
+                f"https://t.me/{channel}/{message.id}",
+                message.views or 0,
+                message.forwards or 0
+            ])
         return rows
 
     async def __aenter__(self):
